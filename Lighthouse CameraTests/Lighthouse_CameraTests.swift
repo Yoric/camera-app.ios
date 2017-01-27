246fab0fe990ee9011560d69f6d5d1ba0c6dd671
David Teller <D.O.Teller@gmail.com>
2017-01-27 18:37:18 +0100

    [WIP] Remove perf test

--- Lighthouse CameraTests/Lighthouse_CameraTests.swift
+++ Lighthouse CameraTests/Lighthouse_CameraTests.swift
@@ -26,12 +26,4 @@ class Lighthouse_CameraTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
       XCTAssert(true, "THIS SHOULD SUCCEED");
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
